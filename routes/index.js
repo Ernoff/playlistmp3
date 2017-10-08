98c696be06b0f2de0bebacd5fe8c6a86ebc04c67
ItachiUchiha1998 <vinayak.shrivastava@gmail.com>
2017-10-09 00:25:53 +0530

    youtube video download added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const youtubedl = require('youtube-dl')
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' })
 })
+
+
+router.post('/download/:link',(req,res,next) => {
+	var link = req.params.link;
+	var video = youtubedl(link);
+
+	video.on('info', function(info) {
+	  console.log('Download started');
+	  console.log('filename: ' + info.filename);
+	  console.log('size: ' + info.size);
+	});
+
+	var i = Math.random();
+ 
+video.pipe(fs.createWriteStream('myvideo'+i+'.mp4'));
+
+}
 
 module.exports = router
