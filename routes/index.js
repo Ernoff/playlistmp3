161051546f3e9470b389ffff8f5ead8be100a5c3
manuasir <manu.asi2@gmail.com>
2017-10-08 21:52:08 +0200

    fixing scope troubles and downloadig locally

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,24 @@
 const express = require('express')
+const fs = require('fs')
 const router = express.Router()
 const youtubedl = require('youtube-dl')
 
-router.get('/download/link',(req,res,next) => {
+router.get('/download/:link',(req,res,next) => {
   let link = "https://www.youtube.com/watch?v="+req.params.link
   console.log(link)
-	let video = youtubedl(link)
+  let video = youtubedl(link)
 
-	video.on('info', function(info) {
-	  console.log('Download started')
-	  console.log('filename: ' + info.filename)
-	  console.log('size: ' + info.size)
-	});
+  video.on('info', (info) => {
+    console.log('Download started')
+    console.log('filename: ' + info.filename)
+    console.log('size: ' + info.size)
+  });
 
-	let i = Math.random()
- 
+  let i = Math.random()
+
   video.pipe(fs.createWriteStream('myvideo'+i+'.mp4'))
 
 })
+
 
 module.exports = router
